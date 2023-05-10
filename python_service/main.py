b2f717408a75d6d01f8c7000e56d4621c8e0f262
gitmazter <philip.andelic@gmail.com>
2023-05-10 15:33:04 +0700

    fixed import for update holders

--- python_service/main.py
+++ python_service/main.py
@@ -1,7 +1,9 @@
 from pymongo import MongoClient
 from services.settings import GET_KEY
+from modules.update_holders.update_holders import update_holders, update_user_transactions
 from modules.supply.supply import update_circulating_supply
 from modules.update_igt_shares.update_igt_shares import update_igt_shares
+
 # GLOBAL VARS
 MONGO_DB_URL = "mongodb+srv://"+ str(GET_KEY("MONGO_DB_UN")) + ":" + str(GET_KEY("MONGO_DB_KEY")) + "@ischost.7b510c2.mongodb.net/?retryWrites=true&w=majority"
 CLUSTER = MongoClient(MONGO_DB_URL)
@@ -13,8 +15,8 @@ transactions_collection =  DB["all_transactions"]
 supply_collection = DB["supply"]
 
 def main():
-    #update_holders(all_holders_collection)
-    #update_user_transactions(all_holders_collection) ## Finished, takes long time to update, rewrite when TX database access is established
+    update_holders(all_holders_collection)
+    update_user_transactions(all_holders_collection) ## Finished, takes long time to update, rewrite when TX database access is established
     circulating_supply = update_circulating_supply(all_holders_collection, supply_collection)
     update_igt_shares(circulating_supply, all_holders_collection, supply_collection)
 

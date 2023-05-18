f516d1d611f34bb194674786108b4e82236e9200
gitmazter <philip.andelic@gmail.com>
2023-05-19 00:51:52 +0700

    403 fix for holder getter

--- python_service/modules/update_igt_shares/update_igt_shares.py
+++ python_service/modules/update_igt_shares/update_igt_shares.py
@@ -13,17 +13,20 @@ def update_igt_shares(all_holders_collection, supply_collection):
 
     total_share = 0
     now = time.time()
+    print("now 1 " + str(now))
 
     for holder in holders:
         validate_user_txs(holder['transactions'])
         share = igt_calc(holder['transactions'], supply_arr, now)
+        if (holder["_id"] ==  "8G46LehJsszbjes5cUZ3M1kXrumiBre2cyRN22opo9HE"):
+            print(share)
         total_share += share
         # myquery = { "_id": holder["_id"]}
         # newvalues = { "$set": { "igtShare": share } }
         # all_holders_collection.update_one(myquery, newvalues)
     all_txs = get_all_txs(all_holders_collection)
-    print(len(all_txs))
     #print(validate_via_timestamps(all_txs))
+    print("now 2 " + str(now))
     verify_shares(total_share, supply_arr, now)
 
 def get_all_txs(holders_col):
--- python_service/services/solscan_getters.py
+++ python_service/services/solscan_getters.py
@@ -2,15 +2,25 @@ import requests
 import json
 import time 
 from services.settings import GET_KEY
+from urllib.request import Request, urlopen
 
 tokenAddress = GET_KEY("TOKEN_ADDRESS")
 mintAuthority = GET_KEY("MINT_AUTHORITY")
-headers = {"token":str(GET_KEY("SOLSCAN_API_KEY"))}  
+apiKey = GET_KEY("SOLSCAN_API_KEY")
+
+headers = {
+    "token": apiKey,
+    'User-Agent': 'Mozilla/5.0'
+    }  
 
 def callHoldersApi(offset): 
-    URI = "https://public-api.solscan.io/token/holders?tokenAddress=" + tokenAddress + "&limit=50&offset=" + str(offset)
-    res = requests.get(URI, headers=headers)
-    response = json.loads(res.text)
+    URI ="https://public-api.solscan.io/token/holders?tokenAddress=" + tokenAddress + "&limit=50&offset=" + str(offset)
+    req = Request(
+        url=URI, 
+        headers=headers
+    )
+    webpage = urlopen(req).read()
+    response = json.loads(webpage)
     return response
 
 def callMetaApi():

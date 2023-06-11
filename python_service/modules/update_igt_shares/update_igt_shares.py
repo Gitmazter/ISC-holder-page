2e95142d02ac47eb9a078eea21bd3ab237cb7504
gitmazter <philip.andelic@gmail.com>
2023-06-11 13:21:44 +0700

    uncomments

--- python_service/modules/update_igt_shares/update_igt_shares.py
+++ python_service/modules/update_igt_shares/update_igt_shares.py
@@ -15,15 +15,15 @@ def update_igt_shares(all_holders_collection, supply_collection):
     now = time.time()
     print("now 1 " + str(now))
 
-    # for holder in holders:
-        # validate_user_txs(holder['transactions'])
-        # share = igt_calc(holder['transactions'], supply_arr, now)
-        # if (holder["_id"] ==  "8G46LehJsszbjes5cUZ3M1kXrumiBre2cyRN22opo9HE"):
-            # print(share)
-        # total_share += share
-        # myquery = { "_id": holder["_id"]}
-        # newvalues = { "$set": { "igtShare": share } }
-        # all_holders_collection.update_one(myquery, newvalues)
+    for holder in holders:
+        validate_user_txs(holder['transactions'])
+        share = igt_calc(holder['transactions'], supply_arr, now)
+        if (holder["_id"] ==  "8G46LehJsszbjes5cUZ3M1kXrumiBre2cyRN22opo9HE"):
+            print(share)
+        total_share += share
+        myquery = { "_id": holder["_id"]}
+        newvalues = { "$set": { "igtShare": share } }
+        all_holders_collection.update_one(myquery, newvalues)
 
     all_txs = get_all_txs(all_holders_collection)
     print("The grand sum of all timestamps is(should be 0 in a perfect world) : ", str(validate_via_timestamps(all_txs)))
@@ -37,7 +37,6 @@ def get_all_txs(holders_col):
 
     for holder in holders:
         all_txs = all_txs + holder['transactions']
-    # print(all_txs)
     return all_txs
 
 

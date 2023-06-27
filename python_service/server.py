675018eb58375a8fe4d25d399a7b00cb14e5f022
gitmazter <philip.andelic@gmail.com>
2023-06-27 18:57:39 +0700

    rm ngrok

--- python_service/server.py
+++ python_service/server.py
@@ -7,7 +7,6 @@ from flask_cors import CORS
 from flask_ngrok import run_with_ngrok
 import time, json, logging
 
-
 # GLOBAL VARS
 MONGO_DB_URL = "mongodb+srv://"+ str(GET_KEY("MONGO_DB_UN")) + ":" + str(GET_KEY("MONGO_DB_KEY")) + "@ischost.7b510c2.mongodb.net/?retryWrites=true&w=majority"
 CLUSTER = MongoClient(MONGO_DB_URL)
@@ -22,7 +21,7 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
 logging.getLogger('flask_cors').level = logging.DEBUG
 CORS(app, resources={r"/*": {"origins": "*"}})
-run_with_ngrok(app)
+# run_with_ngrok(app)
 
 @app.route('/', methods=['POST'])
 def home_page():
@@ -33,8 +32,8 @@ def home_page():
 @app.route('/user/', methods=['POST']) 
 def request_page():
     user_query = request.args.get('address') # /user/?address=yourPubKey
-    holder = all_holders_collection.find({ "_id" :  user_query })[0]
 
+    holder = all_holders_collection.find({ "_id" :  user_query })[0]
     update_single_user_txs(holder, all_holders_collection)
     get_single_share(all_holders_collection, supply_collection, id=user_query)
 
@@ -43,5 +42,6 @@ def request_page():
     json_holder = json.dumps(updated_holder)
     return Response(json_holder)
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(port = 4646)
